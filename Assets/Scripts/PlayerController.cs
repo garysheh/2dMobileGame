01983f42360e57aea96e6665d8722040a5a9cd0a
garysheh <jiayaoshe@gmail.com>
2023-12-12 09:16:13 +0800

    Update from 12/12
    - added jump function
    - added scenes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     public float jumpForce;
 
+    private bool jumpPressed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +22,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        CheckInput();
     }
 
     public void FixedUpdate()
     {
         Movement();
+        Jump();
+    }
+
+    void CheckInput()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+        }
     }
 
     void Movement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1 ~ 1 included decimal
-        // float horizontalInput = Input.GetAxis("Horizontal"); // input delay
+        // float horizontalInput = Input.GetAxis("Horizontal"); // decline movement delay
 
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
 
         if(horizontalInput != 0)
         {
             transform.localScale = new Vector3(horizontalInput, 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        if(jumpPressed)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpPressed = false;
         }
     }
 }
